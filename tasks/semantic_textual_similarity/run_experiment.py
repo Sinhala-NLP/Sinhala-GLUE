4d2c728bfc2be1c0a45230845bdfcc1c83d4f4da
TharinduDR <rhtdranasinghe@gmail.com>
2024-04-18 11:04:00 +0100

    002: Semantic Textual Similarity Experiments

--- tasks/semantic_textual_similarity/run_experiment.py
+++ tasks/semantic_textual_similarity/run_experiment.py
@@ -10,8 +10,8 @@ from config.model_args import TextClassificationArgs
 from tasks.semantic_textual_similarity.evaluation import pearson_corr, spearman_corr, rmse
 from text_classification.text_classification_model import TextClassificationModel
 
-model_name = "NLPC-UOM/SinBERT-small"
-model_type = "roberta"
+model_name = "FacebookAI/xlm-roberta-base"
+model_type = "xlmroberta"
 
 train = Dataset.to_pandas(load_dataset('sinhala-nlp/semantic-textual-similarity', split='train'))
 test = Dataset.to_pandas(load_dataset('sinhala-nlp/semantic-textual-similarity', split='test'))
