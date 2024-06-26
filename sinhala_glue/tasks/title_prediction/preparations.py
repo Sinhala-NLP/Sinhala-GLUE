3996c1fe5b65c264f9e58026e04532f26c950275
TharinduDR <rhtdranasinghe@gmail.com>
2024-06-26 17:16:15 +0100

    001: Restructuring project

--- sinhala_glue/tasks/title_prediction/preparations.py
+++ sinhala_glue/tasks/title_prediction/preparations.py
@@ -5,9 +5,11 @@ from datasets import Dataset
 from datasets import load_dataset
 
 
-def significant_overlap(df, threshold=0.2):
-    # Vectorize the News Content to get token counts
-    vectorizer = CountVectorizer(max_features=16000).fit_transform(df['News Content'])
+def significant_overlap(df, threshold=0.2, max_features=30000):
+
+    # Vectorize the News Content to get token counts with a maximum of 30,000 features
+    vectorizer = CountVectorizer(token_pattern=r'(?u)\b\w{3,}\b', max_features=max_features).fit_transform(
+        df['News Content'])
     vectors = vectorizer.toarray()
 
     # Calculate cosine similarity matrix
@@ -16,25 +18,31 @@ def significant_overlap(df, threshold=0.2):
     # Create a list to store the new rows
     new_rows = []
 
-    for idx, row in df.iterrows():
-        # Find indices of contents with significant overlap
-        similar_indices = [i for i, score in enumerate(cosine_matrix[idx]) if score >= threshold and i != idx]
+    # Iterate over the dataframe with a progress bar
+    for idx, row in tqdm(df.iterrows(), total=df.shape[0], desc="Processing rows"):
+        # Find indices and scores of contents with significant overlap
+        similar_indices_and_scores = [(i, score) for i, score in enumerate(cosine_matrix[idx]) if
+                                      score >= threshold and i != idx]
+
+        # Sort by score in descending order and select top 3
+        similar_indices_and_scores.sort(key=lambda x: x[1], reverse=True)
+        top_similar_indices = [i for i, score in similar_indices_and_scores[:3]]
 
         # If there are any similar contents
-        if similar_indices:
+        if top_similar_indices:
             # Add the original row with is_headline=1
             new_rows.append({
                 'News Content': row['News Content'],
-                'Headline': row['Headline'],
-                'Is_headline': 1
+                'headline': row['headline'],
+                'is_headline': 1
             })
 
             # Add rows with significant overlap
-            for sim_idx in similar_indices:
+            for sim_idx in top_similar_indices:
                 new_rows.append({
                     'News Content': df.at[sim_idx, 'News Content'],
-                    'Headline': row['Headline'],
-                    'Is_headline': 0
+                    'headline': row['headline'],
+                    'is_headline': 0
                 })
 
     # Create the new dataframe
