31da424af9b54d4ffdb273e44a6c4adc58e5b55a
TharinduDR <rhtdranasinghe@gmail.com>
2024-08-15 13:20:23 +0100

    001: Restructuring project

--- sinhala_glue/tasks/comment_popularity_prediction/run_experiment.py
+++ sinhala_glue/tasks/comment_popularity_prediction/run_experiment.py
@@ -11,8 +11,8 @@ from sinhala_glue.config.model_args import TextClassificationArgs
 from sinhala_glue.tasks.comment_popularity_prediction.evaluation import pearson_corr, spearman_corr, rmse
 from sinhala_glue.text_classification.text_classification_model import TextClassificationModel
 
-model_name = "FacebookAI/xlm-roberta-large"
-model_type = "xlmroberta"
+model_name = "NLPC-UOM/SinBERT-large"
+model_type = "roberta"
 
 train = Dataset.to_pandas(load_dataset('sinhala-nlp/sinhala-comment-popularity-prediction', split='train'))
 test = Dataset.to_pandas(load_dataset('sinhala-nlp/sinhala-comment-popularity-prediction', split='test'))
